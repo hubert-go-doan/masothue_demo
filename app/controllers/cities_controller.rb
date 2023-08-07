de8c557c0750d95ba73035f12d8d0d7443341578
hubert-go-doan <hubert.doan.goldenowl@gmail.com>
2023-08-07 09:34:23 +0700

    update list-region

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,36 +1,33 @@
 class CitiesController < ApplicationController
-
   before_action :prepare_cities, only: %i[index show]
 
   def index
   end
 
   def show
-    @city = City.find(params[:id]) 
+    @city = City.includes(:districts, :companies).find(params[:id])
     @districts = @city.districts
-    @companies = @city.companies.shuffle
-    rescue ActiveRecord::RecordNotFound 
-      redirect_to cities_path   
+    @companies = @city.companies.to_a.shuffle
+  rescue ActiveRecord::RecordNotFound
+    redirect_to cities_path
   end
 
   def show_district
-    @district = District.find(params[:id]) 
+    @district = District.includes(:city, :wards, :companies).find(params[:id])
     @city = @district.city
     @wards = @district.wards
-    @companies = @district.companies.shuffle
+    @companies = @district.companies.to_a.shuffle
   end
 
   def show_ward
-    @ward = Ward.find(params[:id]) 
+    @ward = Ward.includes(district: [:city, :wards, :companies]).find(params[:id])
     @city = @ward.district.city
     @district = @ward.district
     @wards = @district.wards
-    @companies = @ward.companies.shuffle
+    @companies = @ward.companies.to_a.shuffle
   end
 
   def prepare_cities
-    @cities =  City.order(:id)
+    @cities = City.order(:id)
   end
 end
-
-
