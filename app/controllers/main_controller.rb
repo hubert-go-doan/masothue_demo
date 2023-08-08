004df2b363db0841870ca52ffa74ddb841c7abfd
hubert-go-doan <hubert.doan.goldenowl@gmail.com>
2023-08-08 17:19:12 +0700

    Update Search && Filter

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,27 +6,18 @@ class MainController < ApplicationController
   end
 
   def search
-    query = params[:q]
-    if query.present?
-      query = query.strip.downcase
-    end
-
+    query = params[:q]&.strip&.downcase
     city_id = params[:city_id]
-
     @results = []
     
     if query.present?
-      @results += Company.where("LOWER(name) LIKE ?", "%#{query}%")
-      @results += Company.joins(:represent).where("LOWER(represents.name) LIKE ?", "%#{query}%")
-      @results += Company.joins(:tax_code).where("tax_codes.code LIKE ?", "%#{query}%")
-
-      @results += Person.where("LOWER(name) LIKE ?", "%#{query}%")
-      @results += Person.joins(:tax_code).where("tax_codes.code LIKE ?", "%#{query}%")
+      @results += Company.where("LOWER(companies.name) LIKE ? OR LOWER(represents.name) LIKE ? OR tax_codes.code LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%").joins(:represent, :tax_code)
+      @results += Person.where("LOWER(people.name) LIKE ? OR tax_codes.code LIKE ?", "%#{query}%", "%#{query}%").joins(:tax_code)
     end
   
     if @results.count == 1
       result = @results.first
-      type = result.instance_of?(Company) ? 'company' : 'person'
+      type = result.class.name.downcase
       redirect_to info_detail_path(id: result.id, type: type)
     else
       render 'search_results'
